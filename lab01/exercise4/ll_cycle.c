7188e12e658490f434c1e784f721e3fb05427e20
Muhammad Ibrahim <145221589+ibrahimshah1998k@users.noreply.github.com>
2023-12-13 12:34:44 +0500

    Update ll_cycle.c

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,22 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    
+    
+    if(head == NULL) return 0;
+    
+    
+	node *fast_ptr = head;
+	node *slow_ptr = head;
+	
+	do{
+		fast_ptr = fast_ptr->next;
+		slow_ptr = slow_ptr->next;
+		if(fast_ptr != NULL) 
+			fast_ptr = fast_ptr->next;
+	}while(slow_ptr != fast_ptr && fast_ptr != NULL);
+	
+	if(slow_ptr == fast_ptr) return 1;
+    return 0;
+
 }
