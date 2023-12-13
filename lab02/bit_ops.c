d9251648df53bdd2aade92f75436bab2d1356485
Muhammad Ibrahim <145221589+ibrahimshah1998k@users.noreply.github.com>
2023-12-13 12:36:04 +0500

    Update bit_ops.c

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include "bit_ops.h"
 
-/* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
+// Return the nth bit of x.
+// Assume 0 <= n <= 31
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
-}
 
-/* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
-void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+	return x>>n&1;
 }
+// Set the nth bit of the value of x to v.
+// Assume 0 <= n <= 31, and v is 0 or 1
+void set_bit(unsigned * x,unsigned n,unsigned v) {
 
-/* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
-void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
+	unsigned a = 1;
+	unsigned b = 0;
+	
+	b = ~b; 
+	b = b<<(n+1);
+	
+
+	v = ~v;
+	v = v << n;
+	v = ~v;
+	v = v|b;
+	
+
+	a = a << n;
+	(*x) = (*x)|a;
+	
+	(*x) = (*x)&v;
 }
+// Flip the nth bit of the value of x.
+// Assume 0 <= n <= 31
+void flip_bit(unsigned * x,unsigned n) {
 
+	(*x) ^= (1 << n);
+}
